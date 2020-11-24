9435ac6852e99ddd243aaf7047dc3f2c1b89dca0
lpellier <pellierlucas@gmail.com>
2020-11-24 16:03:31 +0100

    nginx almost done

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
-su $USER
-docker start minikube --driver=docker
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# su $USER
+minikube start --cpus=4 --driver=docker
+eval $(minikube docker-env)
+docker build -t nginx nginx/
+kubectl apply -f nginx/nginx-deployment.yaml
+kubectl expose deployment nginx-deployment --type=LoadBalancer
+minikube service nginx-deployment
