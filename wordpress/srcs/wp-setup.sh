6eda20645779319635672cd51114d0a00edc767d
Lucas PELLIER <lpellier@e1r12p14.42.fr>
2021-01-19 16:53:55 +0100

    added k8s compatibility for wp/mysql

--- wordpress/srcs/wp-setup.sh
+++ wordpress/srcs/wp-setup.sh
@@ -1,9 +1,26 @@
 echo "198.143.164.252 api.wordpress.org \
 198.143.164.250 downloads.wordpress.org" >> /etc/hosts
 mkdir /home/www/wordpress
+
 wp core download --path=/home/www/wordpress --allow-root
-wp config create --dbname=mysql-wp-db --dbuser=admin --dbuser=pass --dbhost=mysql --path=/home/www/wordpress/ --allow-root
-wp core install --url=127.0.0.1:5050 --title=Example --admin_user=admin --admin_password=pass --admin_email=info@example.com --path=/home/www/wordpress/ --allow-root
+
+wp config create \
+--dbname=mysql-wp-db \
+--dbuser=admin \
+--dbpass=$WORDPRESS_DB_PASSWORD \
+--dbhost=$WORDPRESS_DB_HOST \
+--path=/home/www/wordpress/ \
+--allow-root
+
+wp core install \
+--url=192.168.49.2:5050 \
+--title=Example \
+--admin_user=admin \
+--admin_password=$WORDPRESS_DB_PASSWORD \
+--admin_email=info@example.com \
+--path=/home/www/wordpress/ \
+--allow-root
+
 chown -R www:www /home/www/wordpress
 
 rc-service php-fpm7 restart && nginx -g "daemon off;"
