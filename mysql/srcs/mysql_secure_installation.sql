60afbac2fc073fac3561fd3214efa4e82ae3956b
Lucas PELLIER <lpellier@e1r12p14.42.fr>
2021-01-19 15:19:45 +0100

    mysql almost done

--- mysql/srcs/mysql_secure_installation.sql
+++ mysql/srcs/mysql_secure_installation.sql
@@ -3,4 +3,6 @@ DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+CREATE DATABASE wordpress;
+GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY 'pass';
 FLUSH PRIVILEGES;
--- wordpress/srcs/wp-setup.sh
+++ wordpress/srcs/wp-setup.sh
@@ -2,7 +2,8 @@ echo "198.143.164.252 api.wordpress.org \
 198.143.164.250 downloads.wordpress.org" >> /etc/hosts
 mkdir /home/www/wordpress
 wp core download --path=/home/www/wordpress --allow-root
-wp config create --dbname=mysql-wp-db --dbuser=admin --dbuser=pass --dbhost=mysql --path=/home/www/wordpress/ --url=127.0.0.1:5050 --allow-root
+wp config create --dbname=mysql-wp-db --dbuser=admin --dbuser=pass --dbhost=mysql --path=/home/www/wordpress/ --allow-root
 wp core install --url=127.0.0.1:5050 --title=Example --admin_user=admin --admin_password=pass --admin_email=info@example.com --path=/home/www/wordpress/ --allow-root
+chown -R www:www /home/www/wordpress
 
-nginx -g "daemon off;" && rc-service php-fpm7 restart
+rc-service php-fpm7 restart && nginx -g "daemon off;"
