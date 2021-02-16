fc59dd764767d5c92691b443d53d26b334d5c5ee
oui <you@example.com>
2021-02-16 12:14:49 +0100

    influxdb pod now restarts when process killed

--- srcs/influxdb/srcs/init.sh
+++ srcs/influxdb/srcs/init.sh
@@ -2,4 +2,5 @@ influxd &
 sleep 10 
 influx -execute "create database influx_db"
 influx -execute "create user influx_user with password 'influx_password'"
-telegraf & tail -f /dev/null
+killall influxd influxdb
+telegraf & influxd 
